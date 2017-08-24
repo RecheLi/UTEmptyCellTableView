ed1c5e8b263467b02688b9d6c559dbee2ba09e38
liht <liht@AppledeiMac.local>
2017-08-24 14:57:47 +0800

    Initial Commit

---
+++ UTEmptyCellTableView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UTEmptyCellTableView
+//
+//  Created by liht on 2017/8/24.
+//  Copyright © 2017年 linitial. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UTEmptyCellTableView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UTEmptyCellTableView
+//
+//  Created by liht on 2017/8/24.
+//  Copyright © 2017年 linitial. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
